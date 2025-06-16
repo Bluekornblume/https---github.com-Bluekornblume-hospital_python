afa5f95106062da493020d1302d2bfbbc5b37618
Bluekornblume <kinoko84@outlook.com>
2025-06-16 16:14:02 +0900

    添加config.py到gitignore

--- config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = 'lqg'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:123456@localhost:3306/hospital_db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_ECHO = True
